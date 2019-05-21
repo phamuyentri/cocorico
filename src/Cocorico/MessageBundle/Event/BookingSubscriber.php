fa1aeca0470ae22c47e4218f662064c21e01a7c6
Tri Pham <tri.pham@8bitrockr.com>
2019-05-22 03:48:10 +0700

    3.2 send email to admin while book created

--- src/Cocorico/MessageBundle/Event/BookingSubscriber.php
+++ src/Cocorico/MessageBundle/Event/BookingSubscriber.php
@@ -14,6 +14,7 @@ namespace Cocorico\MessageBundle\Event;
 
 use Cocorico\CoreBundle\Event\BookingEvent;
 use Cocorico\CoreBundle\Event\BookingEvents;
+use Cocorico\CoreBundle\Mailer\TwigSwiftMailer;
 use Cocorico\MessageBundle\Model\ThreadManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -22,12 +23,16 @@ class BookingSubscriber implements EventSubscriberInterface
 {
     protected $threadManager;
 
+    protected $twigSwiftMailer;
+
     /**
      * @param ThreadManager $threadManager
+     * @param TwigSwiftMailer $twigSwiftMailer
      */
-    public function __construct(ThreadManager $threadManager)
+    public function __construct(ThreadManager $threadManager, TwigSwiftMailer $twigSwiftMailer)
     {
         $this->threadManager = $threadManager;
+        $this->twigSwiftMailer = $twigSwiftMailer;
     }
 
 
@@ -36,6 +41,10 @@ class BookingSubscriber implements EventSubscriberInterface
         $booking = $event->getBooking();
         $user = $booking->getUser();
         $this->threadManager->createNewListingThread($user, $booking);
+        $this->twigSwiftMailer->sendMessageToAdmin(
+            sprintf('New booking created with id: %d', $booking->getId()),
+            sprintf('You have a new booking from user: %s', $user->getUsername())
+        );
     }
 
 
